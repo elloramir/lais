cb12e0fdac0c724b23cb9ead66a71ee9bb6f0c3d
elloramir <cripboygit@gmail.com>
2023-12-28 18:12:36 -0300

    feat: login and register templates

--- src/app/urls.py
+++ src/app/urls.py
@@ -6,6 +6,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
 ]
 
 if settings.DEBUG:
--- src/app/views.py
+++ src/app/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 def index(request):
-	return render(request, 'index.html')
+	return render(request, 'login.html')
+
+def login(request):
+	return render(request, 'login.html')
+
+def register(request):
+	return render(request, 'register.html')
