006fbd57902248c6c9fdc4f7ef341bc57469cd06
elloramir <cripboygit@gmail.com>
2023-12-28 17:58:14 -0300

    refactor: new models

--- src/app/forms.py
+++ src/app/forms.py
@@ -1,4 +1,54 @@
 from django import forms
+from django.utils import timezone
 from . import models
 
-# TBD
+NOT_ALLOWED_GROUPS = [
+    "População Privada de Liberdade",
+    "Pessoas com Deficiência Institucionalizadas",
+    "Pessoas ACAMADAS de 80 anos ou mais"
+]
+
+class CandidatoForm(forms.ModelForm):
+	senha = forms.CharField(required=True)
+	confirmacao_senha = forms.CharField(required=True)
+	teve_covid_recentemente = forms.BooleanField(required=True)
+
+	class Meta:
+		model = models.Candidato
+		exclude = ['user']
+
+	def is_valid(self):
+		valid = super(CandidatoForm, self).is_valid()
+		if not valid:
+			return False
+
+		# TODO(ellora): are this considering leap years?
+		# validate "data de nascimento"
+		age = self.cleaned_data['data_nascimento']
+		if 18 > (timezone.now() - age).year
+			return False
+
+		# TODO(ellora): real validation of "CPF"
+		# validate "CPF"
+		cpf = self.cleaned_data['cpf']
+		if not len(cpf) == 11:
+			return False
+		
+		# validate "senha" and "confirmacao_senha"
+		senha = self.cleaned_data['senha']
+		confirmacao_senha = self.cleaned_data['confirmacao_senha']
+		if senha != confirmacao_senha:
+			return False
+
+		# validate "grupos_atendimento"
+		grupos_atendimento = self.cleaned_data['grupos_atendimento']
+		for grupo in grupos_atendimento:
+			if grupo.nome in NOT_ALLOWED_GROUPS:
+				return False
+
+		# validate "teve_covid_recentemente"
+		teve_covid_recentemente = self.cleaned_data['teve_covid_recentemente']
+		if teve_covid_recentemente:
+			return False
+
+		return True
--- src/app/models.py
+++ src/app/models.py
@@ -1,56 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
+# TODO(ellora): populate this table with the XML data
+class GrupoAtendimento(models.Model):
+    nome = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.nome
 
 # Represents candidate data, including personal
 # information and eligibility status for scheduling.
 class Candidato(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     nome_completo = models.CharField(max_length=255)
-    data_nascimento = models.DateField()
     cpf = models.CharField(max_length=11, unique=True)
-    apto_agendamento = models.BooleanField(default=False)
+    data_nascimento = models.DateField()
+    grupos_atendimento = models.ManyToManyField(GrupoAtendimento, blank=True)
 
-
-# Defines groups of attendance available for candidates
-# during self-registration.
-class GrupoAtendimento(models.Model):
-    nome = models.CharField(max_length=255)
-
-
-# Stores data provided during self-registration, including attendance
-# groups and information about COVID-19.
-class Autocadastro(models.Model):
-    candidato = models.OneToOneField(Candidato, on_delete=models.CASCADE)
-    grupos_atendimento = models.ManyToManyField(GrupoAtendimento)
-    teve_covid_recentemente = models.BooleanField()
-    senha = models.CharField(max_length=255)
-
-
-# Represents healthcare facilities available for examination scheduling.
-class EstabelecimentoSaude(models.Model):
-    cnes = models.CharField(max_length=7, unique=True)
-    nome = models.CharField(max_length=255)
-
-
-# Records examination appointments, linking candidates to specific
-# healthcare facilities.
-class Agendamento(models.Model):
-    candidato = models.ForeignKey(Candidato, on_delete=models.CASCADE)
-    estabelecimento_saude = models.ForeignKey(EstabelecimentoSaude, on_delete=models.CASCADE)
-    data_hora_agendamento = models.DateTimeField()
-    agendamento_expirado = models.BooleanField(default=False)
-
-
-# Provides a consolidated view of the number of appointments per
-# facility for the administrative panel.
-class PainelAdministrativo(models.Model):
-    estabelecimento_saude = models.ForeignKey(EstabelecimentoSaude, on_delete=models.CASCADE)
-    quantidade_agendamentos = models.PositiveIntegerField()
-
-
-# Supplies data for charts showing the quantity of users eligible
-# and ineligible for scheduling.
-class GraficoUsuarios(models.Model):
-    quantidade_aptos = models.PositiveIntegerField()
-    quantidade_inaptos = models.PositiveIntegerField()
+    def __str__(self):
+        return self.nome_completo
