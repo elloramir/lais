7b638ad2c5dfdebfe0ac78f9889e734311d4d1e0
elloramir <cripboygit@gmail.com>
2023-12-28 13:46:03 -0300

    refactor: deleted already created forms

--- src/app/forms.py
+++ src/app/forms.py
@@ -1,22 +1,4 @@
 from django import forms
 from . import models
 
-class CandidatoForm(forms.ModelForm):
-    class Meta:
-        model = models.Candidato
-        fields = ['nome_completo', 'data_nascimento', 'cpf']
-
-class AutocadastroForm(forms.ModelForm):
-    class Meta:
-        model = models.Autocadastro
-        fields = ['grupos_atendimento', 'teve_covid_recentemente', 'senha']
-
-class EstabelecimentoSaudeForm(forms.ModelForm):
-    class Meta:
-        model = models.EstabelecimentoSaude
-        fields = ['cnes', 'nome']
-
-class AgendamentoForm(forms.ModelForm):
-    class Meta:
-        model = models.Agendamento
-        fields = ['estabelecimento_saude', 'data_hora_agendamento']
+# TBD
--- src/app/views.py
+++ src/app/views.py
@@ -3,18 +3,3 @@ from django.contrib.auth.decorators import login_required
 
 def index(request):
 	return render(request, 'index.html')
-
-def autocadastro(request):
-    pass
-
-@login_required
-def pagina_de_listagem(request):
-    pass
-
-@login_required
-def agendamento(request):
-    pass
-
-@login_required
-def painel_administrativo(request):
-    pass
