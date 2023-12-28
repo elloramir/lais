519b23541bf77a26676b668f09d591d55a76eb9a
elloramir <cripboygit@gmail.com>
2023-12-28 13:42:58 -0300

    feat: forms e views inicials

--- src/app/views.py
+++ src/app/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 def index(request):
 	return render(request, 'index.html')
+
+def autocadastro(request):
+    pass
+
+@login_required
+def pagina_de_listagem(request):
+    pass
+
+@login_required
+def agendamento(request):
+    pass
+
+@login_required
+def painel_administrativo(request):
+    pass
